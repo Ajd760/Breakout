fda909d0711ccced9533868b7384158c47f01611
Ajd760 <alan.doose@gmail.com>
2022-11-23 22:57:44 -0800

    implement sprite component class

--- source/components/SpriteComponent.cpp
+++ source/components/SpriteComponent.cpp
@@ -1,22 +1,46 @@
 #include "SpriteComponent.h"
 
+SpriteComponent::SpriteComponent(std::string textureID) : textureID(textureID)
+{
+	// TODO: add something to load the texture file if it is not already loaded?
+	this->setTexture(textureID);
+}
+
+// called whenever a component is added to a gameObject in addComponent(TArgs&&... args) 
 void SpriteComponent::init()
 {
+	transform = owner->getComponentByType<TransformComponent>();
+	srcRect.x = 0;
+	srcRect.y = 0;
+	srcRect.w = transform->getTransformWidth(); // set the sprite dimensions equal to the transform
+	srcRect.h = transform->getTransformHeight();
 }
 
 void SpriteComponent::update(float deltaTime)
 {
+	dstRect.x = (int)transform->getTransformPosition().x; // keep the rendering target position with the transform of the gameObject
+	dstRect.y = (int)transform->getTransformPosition().y;
+	dstRect.w = (int)(transform->getTransformWidth() * transform->getTransformScale()); // keep the sprite scaled equal to the transform
+	dstRect.h = (int)(transform->getTransformHeight() * transform->getTransformScale());
+	//std::cout << "dstRect-x: " << this->dstRect.x << std::endl;
+	//std::cout << "dstRect-y: " << this->dstRect.y << std::endl;
+	//std::cout << "dstRect-w: " << this->dstRect.w << std::endl;
+	//std::cout << "dstRect-h: " << this->dstRect.h << std::endl;
 }
 
 void SpriteComponent::render()
 {
-	//How to handle setting the x,y position of the texture? Can we do something like this->owner->getTransform().x?
-	//glm::vec2 position = this->owner->getComponentByType("TransformComponent")->getTransformPosition();
-	//texture->render(position.x, position.y, Game::mainRender);
+	// TODO: modify this if we add angle/centerpoint to the sprite component
+	LTexture::draw(Game::mainRender, texture, &srcRect, &dstRect, 0.0, NULL, this->flip);
+	//std::cout << "we rendered the sprite with textureID: " << this->textureID << std::endl;
 }
 
 std::string SpriteComponent::getType() const
 {
-	//std::string componentType = "SpriteComponent";
 	return std::string("SpriteComponent");
+}
+
+void SpriteComponent::setTexture(std::string textureID)
+{
+	this->texture = Game::assManager->getTexture(textureID);
 }
--- source/components/SpriteComponent.h
+++ source/components/SpriteComponent.h
@@ -1,18 +1,25 @@
 #pragma once
 #include "Component.h"
-#include "../graphics/LTexture.h"
-#include "../GameObject.h"
+#include "TransformComponent.h"
 #include "../Game.h"
 
 class SpriteComponent : public Component
 {
 public:
+	SpriteComponent(std::string textureID);
 	void init() override;
-	//void init(); //with params?
 	void update(float deltaTime) override;
 	void render() override;
 	std::string getType() const override;
+	void setTexture(std::string textureID);
+
+	SDL_RendererFlip flip = SDL_FLIP_NONE; //default to no flip
 protected:
-	LTexture* texture;
+	SDL_Texture* texture;
+	std::string textureID;
+	SDL_Rect srcRect;
+	SDL_Rect dstRect;
+	TransformComponent* transform; // keep track of the transform of the gameObject this is attached to
+	
 };
 
