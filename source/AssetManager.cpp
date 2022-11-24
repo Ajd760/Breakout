052a28630d4988bf2ce8236aba275ed406df932e
Ajd760 <alan.doose@gmail.com>
2022-11-23 22:58:43 -0800

    implement AssetManager

--- source/AssetManager.cpp
+++ source/AssetManager.cpp
@@ -1,1 +1,42 @@
 #include "AssetManager.h"
+#include "Game.h"
+
+AssetManager::AssetManager(GameObjectManager* manager) : gameObjManager(manager)
+{
+}
+
+AssetManager::~AssetManager()
+{
+	destroyEverything();
+	gameObjManager = nullptr;
+}
+
+void AssetManager::destroyEverything()
+{
+	//TODO: is this missing anything?
+	textures.clear();
+}
+
+void AssetManager::addTexture(std::string textureID, std::string path)
+{
+	SDL_Texture* tex = LTexture::loadTextureFromFile(path, Game::mainRender);
+	if (tex != NULL)
+	{
+		textures.emplace(textureID, tex);
+	}
+	else
+	{
+		std::cout << "Error trying to load texture: " << textureID << " from path: " << path << std::endl;
+	}
+}
+
+SDL_Texture* AssetManager::getTexture(std::string textureID) const
+{
+	if (this->textures.at(textureID) != nullptr)
+		return this->textures.at(textureID);
+	else
+	{
+		std::cerr << textureID << std::endl;
+		return nullptr;
+	}
+}
--- source/AssetManager.h
+++ source/AssetManager.h
@@ -4,22 +4,22 @@
 #include <SDL.h>
 
 #include "GameObjectManager.h"
+#include "graphics/LTexture.h"
 
 
 // this class is to hold and track loaded assets, i.e. textures/sounds/etc
 class AssetManager
 {
 public:
-	AssetManager(GameObjectManager* gameObjManager);
+	AssetManager(GameObjectManager* manager);
 	~AssetManager();
 	void destroyEverything();
-	bool addTexture(std::string textureID, std::string path);
+	void addTexture(std::string textureID, std::string path);
 	SDL_Texture* getTexture(std::string textureID) const;
 private:
 	GameObjectManager* gameObjManager;
 	//need to revisit std::map but seems best for holding a 2 column structure 
 	// of textureIDs mapped to the loaded texture
 	std::map<std::string, SDL_Texture*> textures;
-
 };
 
